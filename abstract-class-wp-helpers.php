dc8d5ca846b571060a9dfc426301db2252fd7f79
Piotr Szczygiel <piotr.szczygiel@makingwaves.pl>
2014-01-06 18:05:02 +0100

    added enqueue_script method

--- abstract-class-wp-helpers.php
+++ abstract-class-wp-helpers.php
@@ -10,10 +10,48 @@
  * @author Piotr Szczygiel <psz.szczygiel@gmail.com>
  * @link https://github.com/piotr-szczygiel/wp-helpers - check out latest version
  * @license MIT
- * @version 1.0
+ * @version 1.1
  */
 abstract class wp_helpers
 {
+    // JS file prefix
+    const MIN_PREFIX = '-min';
+    // path for plugin's JS files
+    const JS_ASSETS_PATH = 'assets/js/';
+
+    /**
+     * Method can be used when you want to use packed JavaScript files.
+     * How to use it:
+     * - create 'unpacked' folder under plugin's 'assets/js' folder
+     * - create a new file under 'unpacked' folder and name it something like 'test.js'
+     * - put some content into your file
+     * - copy all the content and generate minified content using tool http://dean.edwards.name/packer/
+     * - create file 'test-min.js' under 'assets/js' and paste the minified content there
+     * - next step is to configure your WP to debug into file by settings following flags in wp-config.php file:
+     *   - define( 'WP_DEBUG', true );
+     *   - define( 'WP_DEBUG_LOG', true );
+     *   - define( 'WP_DEBUG_DISPLAY', false );
+     * You can also use only minified files (f.e third party plugins) by setting $debug parameter as true.
+     * @param string $identifier
+     * @param string $file
+     * @param string $plugin_file
+     * @param boolean $debug
+     * @param array $dep
+     */
+    protected function enqueue_script( $identifier, $file, $plugin_file, $debug = true, $dep = array( 'jquery' ) )
+    {
+        // debug mode
+        if ( $debug === true && WP_DEBUG === true && WP_DEBUG_LOG === true && WP_DEBUG_DISPLAY === false ) {
+            $file_path = 'unpacked/' . str_replace( self::MIN_PREFIX, '', $file );
+        }
+        // live mode
+        else {
+            $file_path = $file;
+        }
+
+        wp_enqueue_script( $identifier, plugins_url( self::JS_ASSETS_PATH . $file_path, $plugin_file ), $dep );
+    }
+
     /**
      * Registering an ajax functions. Method supports both: admin and non-admin 
      * user typing.
